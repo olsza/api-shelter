784e690c0545afd37ddfcb98650fd2a4218dbc86
Olsza <olsza@users.noreply.github.com>
2023-06-19 22:46:07 +0200

    refactoring - use ApiResourceTrait in CatController

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -2,59 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Trait\ApiResourceTrait;
 use App\Models\Cat;
-use Illuminate\Http\Request;
 
 class CatController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return response()->json(
-            Cat::all()
-        );
-    }
+    use ApiResourceTrait;
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $data = Cat::create($request->all());
-
-        return response()->json($data, 201);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        $data = Cat::find($id);
-
-        return response()->json($data, 200);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id)
-    {
-        $data = Cat::findOrFail($id);
-        $data->update($request->all());
-
-        return response()->json($data);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        Cat::destroy($id);
-
-        return response()->json(null, 204);
-    }
+    protected string $modelName = Cat::class;
 }
