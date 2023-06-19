d9facdfa27a200b01ebf89389f783aeff1c68859
Olsza <olsza@users.noreply.github.com>
2023-06-19 22:46:20 +0200

    refactoring - use ApiResourceTrait in EmployeeController

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,59 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Trait\ApiResourceTrait;
 use App\Models\Employee;
-use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return response()->json(
-            Employee::all()
-        );
-    }
+    use ApiResourceTrait;
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $data = Employee::create($request->all());
-
-        return response()->json($data, 201);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        $data = Employee::find($id);
-
-        return response()->json($data, 200);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id)
-    {
-        $data = Employee::findOrFail($id);
-        $data->update($request->all());
-
-        return response()->json($data);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        Employee::destroy($id);
-
-        return response()->json(null, 204);
-    }
+    protected string $modelName = Employee::class;
 }
