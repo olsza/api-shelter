8c8ddd6290164115e3b0c63011af597de4dd0c43
Olsza <olsza@users.noreply.github.com>
2023-06-20 10:07:51 +0200

    refactoring - use Shelter request in Controller

--- app/Http/Controllers/ShelterController.php
+++ app/Http/Controllers/ShelterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Trait\ApiResourceTrait;
+use App\Http\Requests\ShelterRequest;
 use App\Models\Shelter;
 
 class ShelterController extends Controller
@@ -10,4 +11,25 @@ class ShelterController extends Controller
     use ApiResourceTrait;
 
     protected string $modelName = Shelter::class;
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ShelterRequest $request)
+    {
+        $data = $this->modelName::create($request->all());
+
+        return response()->json($data, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ShelterRequest $request, $id)
+    {
+        $data = $this->modelName::findOrFail($id);
+        $data->update($request->all());
+
+        return response()->json($data);
+    }
 }
