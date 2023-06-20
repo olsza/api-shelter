97929617abf8975603fb13516e7ce37d68f4037a
Olsza <olsza@users.noreply.github.com>
2023-06-20 10:07:09 +0200

    refactoring - use Employee request in Controller

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Trait\ApiResourceTrait;
+use App\Http\Requests\EmployeeRequest;
 use App\Models\Employee;
 
 class EmployeeController extends Controller
@@ -10,4 +11,25 @@ class EmployeeController extends Controller
     use ApiResourceTrait;
 
     protected string $modelName = Employee::class;
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(EmployeeRequest $request)
+    {
+        $data = $this->modelName::create($request->all());
+
+        return response()->json($data, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(EmployeeRequest $request, $id)
+    {
+        $data = $this->modelName::findOrFail($id);
+        $data->update($request->all());
+
+        return response()->json($data);
+    }
 }
