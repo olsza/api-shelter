9153cb91baf12c98d83f0405c8cce4fa6d5e735f
Olsza <olsza@users.noreply.github.com>
2023-06-20 10:06:47 +0200

    refactoring - use Cat request in Controller

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Trait\ApiResourceTrait;
+use App\Http\Requests\CatRequest;
 use App\Models\Cat;
 
 class CatController extends Controller
@@ -10,4 +11,25 @@ class CatController extends Controller
     use ApiResourceTrait;
 
     protected string $modelName = Cat::class;
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CatRequest $request)
+    {
+        $data = $this->modelName::create($request->all());
+
+        return response()->json($data, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CatRequest $request, $id)
+    {
+        $data = $this->modelName::findOrFail($id);
+        $data->update($request->all());
+
+        return response()->json($data);
+    }
 }
